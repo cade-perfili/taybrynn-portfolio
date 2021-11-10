972a449a34352fa979df5ecb5bb936e91642a59c
cade-perfili <58614773+cade-perfili@users.noreply.github.com>
2021-11-10 15:44:50 -0500

    Polished Slide show, put JS script at bottom of HTML Document

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,29 @@
+//Slide Show JS
+var slideIndex = [1,1];
+var slideId = ["bfa-slideshow", "personal-slideshow"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
+//Responsive Nav JS
 function myFunction() {
-    var x = document.getElementById("RespNav");
-    if (x.className === "nav") {
-      x.className += " responsive";
-    } else {
-      x.className = "nav";
-    }
-  } 
+  var x = document.getElementById("RespNav");
+  if (x.className === "nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav";
+  }
+} 
